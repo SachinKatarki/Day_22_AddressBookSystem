4f726b93c19e1e5e707942694dd7aa2338de636d
Textspot <sachinkatarki1995@gmail.com>
2021-11-23 23:17:46 +0530

    Added:Use Console to edit Person details in Using Collections

--- src/com/bridgelabz/collections/AddressBookMain.java
+++ src/com/bridgelabz/collections/AddressBookMain.java
@@ -1,11 +1,21 @@
 package com.bridgelabz.collections;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	 public static void main(String[] args) {
-		 System.out.println("Welcome into the Program of Address Book System !!");
+		 Scanner sc=new Scanner(System.in);
+	        System.out.println("Welcome into the Program of Address Book System !!");
 	        NewAddressBook person = new NewAddressBook();
 	        person.addContact();
+	        System.out.println("Enter Y To Edit The Contact");
+	        String op = sc.nextLine();
+	        if (op.equals("y") || op.equals("Y")) {
+	            person.editContact(); //Calling The Edit Contact Method
+	            System.out.println("You have Entered following data");
+	            System.out.println("The Contact Details After Editing : " + person);
+	        }
 
 	    }
 
-}
+     }
