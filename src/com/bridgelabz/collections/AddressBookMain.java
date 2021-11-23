97d414c1e8a75db9232dd37ce6511399646b8fb9
Textspot <sachinkatarki1995@gmail.com>
2021-11-23 23:07:21 +0530

    Added:Using Collections Added new Contact in Adress Book

--- src/com/bridgelabz/collections/AddressBookMain.java
+++ src/com/bridgelabz/collections/AddressBookMain.java
@@ -2,16 +2,9 @@ package com.bridgelabz.collections;
 
 public class AddressBookMain {
 	 public static void main(String[] args) {
-	        System.out.println("Welcome into the Program of Address Book System !!");
-	        ContactDetails newContact = new ContactDetails();
-	        newContact.setFirstName("Sachin\n");
-	        newContact.setLastName("Kumar\n");
-	        newContact.setAddressCity("Bagalkot\n");
-	        newContact.setState("Karanatka\n");
-	        newContact.setEmail("Sachinkumar@gmail.com");
-	        newContact.setZip(414501);
-	        newContact.setPhoneNumber( 9604312330L);
-	        System.out.println("The Contact is :\n" +newContact);
+		 System.out.println("Welcome into the Program of Address Book System !!");
+	        NewAddressBook person = new NewAddressBook();
+	        person.addContact();
 
 	    }
 
